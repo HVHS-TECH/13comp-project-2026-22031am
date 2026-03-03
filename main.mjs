a1d87e7ba8ba179693803566cbb49436d24fdb0c
22031am <22031am@hvhs.school.nz>
2026-03-03 23:30:29 +0000

    save!

---
+++ main.mjs
@@ -0,0 +1,26 @@
+/**************************************************************/
+// main.mjs
+// Main entry for index.html
+// Written by Aditi Modi <Your Name Here>, Term 1 2026?
+/**************************************************************/
+
+const COL_C = 'white';	   	
+const COL_B = '#CD7F32';	
+console.log('%c main.mjs', 
+    'color: blue; background-color: white;');
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the constants & functions required from fb_io module
+
+import {fb_initialise, fb_authenticate, fb_detectLogin } from './fb_io.mjs';
+
+fb_initialise();
+
+window.fb_authenticate = fb_authenticate;
+window.fb_detectLogin  = fb_detectLogin;
+
+/**************************************************************/
+//   END OF CODE
+/**************************************************************/
