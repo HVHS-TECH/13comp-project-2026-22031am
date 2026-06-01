5364a504dd2acaefa011f45589d1fc9fc44a2dc0
22031am <22031am@hvhs.school.nz>
2026-06-01 21:54:21 +0000

    save end of 6/02/26 classs

--- games/gtn_game.mjs
+++ games/gtn_game.mjs
@@ -22,8 +22,47 @@ function checkGuess() {
 
     /*******************************************************/
     // GET THE HTML ELEMENTS
+
     /*******************************************************/
 
     const guessInput = document.getElementById("guess-input");
 
+    const resultBox = document.getElementById("result-box");
+
+    /*******************************************************/
+    // GET THE PLAYER GUESS 
+    /*******************************************************/
+    let guess = Number(guessInput.value);
+    console.log("Player guessed: " + guess);
+
+    /*******************************************************/
+    // CHECK THE PLAYER'S GUESS
+    /*******************************************************/
+
+    if (guess === secretNumber) {
+
+        resultBox.innerHTML = "Correct! You guessed the number! Congratulations!";
+
+    } else if (guess < secretNumber) {
+
+        resultBox.innerHTML = "Too low! Please try again";
+
+    } else {
+
+        resultBox.innerHTML = "Too high! Please try again, you can have another try";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+/*******************************************************/
+window.checkGuess = checkGuess; // Making the function accessible globally so it can be called from the HTML button
+/*******************************************************/
 
