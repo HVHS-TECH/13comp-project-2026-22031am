03242916d46cf93ba99b57c443f27b4fcc3ab158
22031am <22031am@hvhs.school.nz>
2026-06-08 21:33:06 +0000

    SAVEEE

--- games/gtn_leaderboard.mjs
+++ games/gtn_leaderboard.mjs
@@ -12,30 +12,6 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
-/******************************************************/
-// fb_initialise()
-/******************************************************/
-function fb_initialise() {
-    console.log('%c fb_initialise(): ',
-                 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
-                 
-    const FB_GAMECONFIG = {
-      apiKey: "AIzaSyDWhGSciprdeHTyBrXQYt_F-6tMMjCg-YM",
-      authDomain: "comp-2026-aditi-modi.firebaseapp.com",
-      databaseURL: "https://comp-2026-aditi-modi-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "comp-2026-aditi-modi",
-      storageBucket: "comp-2026-aditi-modi.firebasestorage.app",
-      messagingSenderId: "153559444524",
-      appId: "1:153559444524:web:7144aeb2795255e4e1b589",
-      measurementId: "G-0KELMX0418"
-    };
-
-    const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
-    FB_GAMEDB = getDatabase(FB_GAMEAPP);
-    FB_GAMEAUTH = getAuth(FB_GAMEAPP);
-    
-    console.info(FB_GAMEDB);      	//DIAG
-}
 
 /******************************************************/
 // fb_initialise()
@@ -83,7 +59,7 @@ const leaderboardRef = ref(database, "GTN/Leaderboard");
         leaderboardBody.innerHTML = "";
         if (!leaderboardData) {
 
-            leaderboardBody.innerHTML  `
+            leaderboardBody.innerHTML = `
             <tr>
                 <td colspan="3">
                 No leaderboard data yet...
